d215e1e66a5b72e963b8a180248ac4fd1573b4c8
panguanzhen <panguanzhen@qq.com>
2020-04-17 23:48:33 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import 'index.scss';
+import {HashRouter as Router, Link, Redirect, Route, Switch,} from 'react-router-dom';
+import Nav from './components/Nav';
+
+
+
+const Wrapper =styled.div`
+  border: 1px solid red;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+const Main =styled.div`
+     
+     flex-grow: 1;
+     overflow: auto;
+`;
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Wrapper>
+      <Main>
+      <Switch>
+        <Route path="/tags">
+          <Tags/>
+        </Route>
+        <Route path="/money">
+          <Money />
+        </Route>
+        <Route path="/statistics">
+          <Statistics />
+        </Route>
+        <Redirect exact from="/"  to="/money"/>
+        <Route path="*">
+          <NoMatch/>
+        </Route>
+      </Switch>
+      </Main>
+        <Nav/>
+
+
+
+      </Wrapper>
+    </Router>
   );
+
+}
+function Tags() {
+  return <h2>标签页</h2>;
+}
+
+function Money() {
+  return <h2>记账页</h2>;
+}
+
+function Statistics() {
+  return <h2>统计</h2>;
+}
+function NoMatch() {
+  return <h2>地址错误</h2>;
 }
 
 export default App;
